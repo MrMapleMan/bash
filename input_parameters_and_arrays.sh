13d05478c44e0610417039c7ea5f0ad130e7bcab
John Henrie <gimmeyerjunkmail@gmail.com>
2024-03-01 10:39:34 -0700

    arrays

---
+++ input_parameters_and_arrays.sh
@@ -0,0 +1,26 @@
+!/bin/bash
+
+echo $@
+echo $0
+echo ${@:0:20}
+
+
+echo "" > /tmp/txt-test
+
+for i in {0..99};
+do
+  printf "%03d\n" $i >> /tmp/txt-test;
+done;
+
+
+a=(1 2 3 4 5)
+echo ${a[@]:0:3}
+echo ${a[4]}
+
+mapfile -t lines_array < /tmp/txt-test
+index=0
+for line in ${lines_array[@]};
+do
+    printf "%03d %s\n" $index $line;
+    index=$((index+1));
+done;
