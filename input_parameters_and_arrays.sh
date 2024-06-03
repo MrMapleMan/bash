bfc67c28decd41dacd0e0ad6f3543e1459e1e135
John Henrie <gimmeyerjunkmail@gmail.com>
2024-06-03 09:24:10 -0600

    expand array script

--- input_parameters_and_arrays.sh
+++ input_parameters_and_arrays.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 echo $@
 echo $0
@@ -16,6 +16,28 @@ done;
 a=(1 2 3 4 5)
 echo ${a[@]:0:3}
 echo ${a[4]}
+
+b=($0)
+b+=($@)
+i=0
+for element in "${b[@]}"; do
+  echo $i $element
+  i=$((i+1))
+done
+
+array_length=${#a[@]}
+echo "array length: $array_length"
+
+echo b: $b
+echo b[@]: ${b[@]}
+# To append elements to a bash array, you can use the following syntax:
+my_array=(element1 element2)
+my_array+=("new_element")
+
+# Print the updated array
+for element in "${my_array[@]}"; do
+    echo "$element"
+done
 
 mapfile -t lines_array < /tmp/txt-test
 index=0
